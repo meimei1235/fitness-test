809646854ed9ac1f65754b1d0d8d6c6e1a23f28a
meimei1235 <190512054@qq.com>
2017-08-09 15:30:08 +0800

    重构了test2

--- test2/main.js
+++ test2/main.js
@@ -1,14 +1,15 @@
-function fib(num) {
+//第一种算法：不使用递归
+function fib1(num) {
     var arr = [];
     if(num == 1){
-        return arr = [1];
+        return arr = [0];
     }else if(num == 2){
-        return arr = [1,1];
+        return arr = [0,1];
     }else{
-        arr = [1,1];
-        var a=1;
+        arr = [0,1];
+        var a=0;
         var b=1;
-        var c=2;
+        var c=1;
         var arrlen =arr.length;
         for(var i=2;i<num;i++){
             arr.push(c);
@@ -21,4 +22,13 @@ function fib(num) {
     }
 
 }
-console.log(fib(10)+"...");
+//第二种算法：使用递归
+let arr = [];
+function fib2(num) {
+    return num < 1 ? 0 : (num < 3 ? 1: fib2(num - 1) + fib2(num - 2));
+}
+for(let i = 0; i < 10; i++) {
+    arr.push(fib2(i));
+}
+console.log(arr + "...");
+console.log(fib1(10)+"...");
