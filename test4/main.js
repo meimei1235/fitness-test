25e8678b21f0abdbc018a7de00e5d189c1057849
meimei1235 <190512054@qq.com>
2017-08-11 14:48:36 +0800

    add test4

---
+++ test4/main.js
@@ -0,0 +1,16 @@
+//题目：给出链表 1->2->3->3->4->5->3, 和 val = 3, 你需要返回删除3之后的链表：1->2->4->5。
+function chain(str,val){
+    let arr = str.split("->");
+    let result = [];
+    arr.forEach(item => {
+        if(parseInt(item) !== val) {
+            result.push(item);
+        }
+    });
+    let chain = result.join("->");
+    return chain;
+
+}
+let str = '1->2->3->3->4->5->3';
+let a = chain(str, 3 );
+console.log("输入：" + str + "\n" + "输出：" + a);
