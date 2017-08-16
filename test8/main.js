943ebb4a1b8b9f3ed4a5f536941cd6542682d024
meimei1235 <190512054@qq.com>
2017-08-16 15:19:56 +0800

    新增一种方法

--- test8/main.js
+++ test8/main.js
@@ -1,10 +1,21 @@
 //题目：找出数组 arr 中重复出现过的元素
 //例如：arr = [‘a’, ‘ab’, ‘ed’, ‘da’, ‘a’, ‘ed’, ‘b’];输出：‘a’, ‘ed’
 function find_same_elenment(arr) {
+    /*方法1：使用过滤器实现
     let result = arr.filter((elem, index, arr) => {
         return arr.indexOf(elem) != index;
     });
-    return result;
+    //方法2：使用for循环
+    return result;*/
+    let result = [];
+    let arr1 = arr;
+    arr1.sort();
+    arr1.forEach(item => {
+       if(arr1.indexOf(item) != arr1.lastIndexOf(item) && result.indexOf(item) == -1) {
+           result.push(item);
+       }
+   });
+   return result;
 }
 let arr = ['a', 'ab', 'ed', 'da', 'a', 'ed', 'b'];
 let b = find_same_elenment(arr);
