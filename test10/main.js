1758e2b4903708efdf6ff458bcb3069c31faeb0a
meimei1235 <190512054@qq.com>
2017-08-18 14:40:53 +0800

    add test10

---
+++ test10/main.js
@@ -0,0 +1,16 @@
+//在数组 arr 中，查找值与 item 相等的元素出现的所有位置
+//例如：arr = [1, 3, 7, 1 ,4,] , item = 1，输出：0, 3
+//要求：不要使用for，while循环
+function find_elem_index(arr, item) {
+    let result = [];
+    arr.filter((elem, index, arr) => {
+       if (elem === item) {
+           result.push(index);
+        }
+       // return arr.indexOf(item);
+    });
+     return result;
+}
+let arr = [1, 3, 7, 1 ,4,] ;
+let a = find_elem_index(arr, 1);
+console.log("输入：" + arr + "\n" + "输出：" + a);
